85a04bcaba5537b53f37902a5c4b0b8fafd2787b
frknelm <96985839+frknelm@users.noreply.github.com>
2022-02-10 21:52:48 +0100

    Loves JS
    I exhanged the JS files to the ones Love revised during our supervision, hope it works

--- script.js
+++ script.js
@@ -13,23 +13,12 @@
   chromium based web browsers. It will _not_ work in Safari or Firefox.
   See: https://developer.mozilla.org/en-US/docs/Web/API/Web_Serial_API
 
-  - Love Lagerkvist, 220117, Malmö Universitet
+  - Love Lagerkvist, 220117, MalmÃ¶ Universitet
 */
-const state = {
-  dataToWrite: {
-    brightness: 255, //changed from 0 to 255
-  },
-  serial: null,
-  joystick: {
-    x: 0,
-    // y: 0,
-    // pressed: false,
-  },
-}
 
 if (!("serial" in navigator)) {
   alert("Your browser does not support Web Serial, try using something Chromium based.")
-} //You're using the wrong browser!
+}
 
 
 const requestPortButton = document.querySelector("#request-port-access");
@@ -40,217 +29,220 @@ requestPortButton.addEventListener("pointerdown", async (event) => { // note tha
   // Then, open communications to is with the correct baudRate. This _has_ to be the same in both the Arduino sketch and on the website!
   await state.serial.open({ baudRate: 9600 });
 
-  document.querySelector("#connection-status").innerHTML = "Arduino is connected!"; //"product name" connected!
+  document.querySelector("#connection-status").innerHTML = "Arduino is connected!";
 
   // We start the reader function, an async loop that gets data from serial (if any) and then calls the callback
   readJSONFromArduino(async () => {
+    // NOTE This is where we can put our code that runs after the joystick has been read
     updateDataDisplay();
-    if (state.joystick.x > 0) { //If joystick is moved to the right
-    }
-    if (state.joystick.x < 0) { // If joystick is moved to the left
-      //   writeJoystickBrightnessToArduino();
-      // }
+    if (state.joystick.pressed) {
+      writeJoystickBrightnessToArduino();
     }
   });
 });
 
-  // We need an object operating the cards in relation to updateDataDisplay
 
-  // This function reads data from the Arduino and calls the callback, if any.
-  const readJSONFromArduino = async (callback) => {
-    if (!state.serial) throw new Error("No Arduino connected to read the data from!"); // Issue with reading the data (Retr). "Product name failed to connect!"
+// This function reads data from the Arduino and calls the callback, if any.
+const readJSONFromArduino = async (callback) => {
+  if (!state.serial) throw new Error("No Arduino connected to read the data from!");
 
-    // This part is a bit more complex, but you can safely "hand wave it".
-    // I explain it in some depth in the demo.
-    const textDecoder = new TextDecoderStream();
-    const readableStreamClosed = state.serial.readable.pipeTo(textDecoder.writable);
-    const reader = textDecoder.readable.getReader();
-    let lineBuffer = "";
+  // This part is a bit more complex, but you can safely "hand wave it".
+  // I explain it in some depth in the demo.
+  const textDecoder = new TextDecoderStream();
+  const readableStreamClosed = state.serial.readable.pipeTo(textDecoder.writable);
+  const reader = textDecoder.readable.getReader();
+  let lineBuffer = "";
 
-    // Listen to data coming from the serial device.     // How do we need this to for our version to function? 
-    while (state.serial.readable) {
-      const response = await reader.read();
+  // Listen to data coming from the serial device.
+  while (state.serial.readable) {
+    const response = await reader.read();
 
-      if (response.done) {
-        reader.releaseLock();
-        break;
-      }
+    if (response.done) {
+      reader.releaseLock();
+      break;
+    }
 
-      // Again, a bit more complex. We have to manully handle the response
-      // from the Arduino. See the demo.
-      lineBuffer += response.value;
-      const lines = lineBuffer.split("\n");
-      if (lines.length > 1) {                   // We have a complete JSON response!
-        lineBuffer = lines.pop();               // Set the buffer to any data from the next response
-        const line = lines.pop().trim();        // Get the JSON and remove the newline
-        state.joystick = JSON.parse(line);      // Parse the JSON and put it in the state under joystick
-        if (callback) callback();               // Run the callback function, if any
-      }
+    // Again, a bit more complex. We have to manully handle the response
+    // from the Arduino. See the demo.
+    lineBuffer += response.value;
+    const lines = lineBuffer.split("\n");
+    if (lines.length > 1) {                   // We have a complete JSON response!
+      lineBuffer = lines.pop();               // Set the buffer to any data from the next response
+      const line = lines.pop().trim();        // Get the JSON and remove the newline
+      state.joystick = JSON.parse(line);      // Parse the JSON and put it in the state under joystick
+      if (callback) callback();               // Run the callback function, if any
     }
   }
-  // How do we need to modify this to only respond to x values/ how / in what way is the lineBuffer relevant to what we want to achieve? 
+}
 
-  // Write to the Arduino and run the callback, if any
-  const writeJSONToArduino = async (callback) => {
-    if (!state.serial) throw new Error("No Arduino connected to write the data to!"); //Issue in writing the data (Recieve)
 
-    const data = state.dataToWrite; // First, we get the object an object and turn it into JSON.
-    const json = JSON.stringify(data); // Transform our internal JS object into JSON representation, which we store as a string
+// Write to the Arduino and run the callback, if any
+const writeJSONToArduino = async (callback) => {
+  if (!state.serial) throw new Error("No Arduino connected to write the data to!");
 
-    // The serial writer will want the data in a specific format, which we can do with the TextEncoder object, see https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder
-    const payload = new TextEncoder().encode(json);
+  const data = state.dataToWrite; // First, we get the object an object and turn it into JSON.
+  const json = JSON.stringify(data); // Transform our internal JS object into JSON representation, which we store as a string
 
-    // Get the writer, write to it and then release it for the next write
-    const writer = await state.serial.writable.getWriter();
-    await writer.write(payload);
-    writer.releaseLock();
+  // The serial writer will want the data in a specific format, which we can do with the TextEncoder object, see https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder
+  const payload = new TextEncoder().encode(json);
 
-    if (callback) callback(); // Run the callback function, if any
+  // Get the writer, write to it and then release it for the next write
+  const writer = await state.serial.writable.getWriter();
+  await writer.write(payload);
+  writer.releaseLock();
+
+  if (callback) callback(); // Run the callback function, if any
+}
+
+
+// Take the joystick data, map it to the LEDs range and and shuffle it over to the writer
+// NOTE This is the place where we would set the .red .green & .blue
+const writeJoystickBrightnessToArduino = async () => {
+  state.dataToWrite.green = 0; // NOTE replace these numbers with whatever you want the RGB to be
+  state.dataToWrite.red = 0;   // NOTE replace these numbers with whatever you want the RGB to be
+  state.dataToWrite.blue = 0;  // NOTE replace these numbers with whatever you want the RGB to be
+  writeJSONToArduino();
+}
+
+
+// Simple monitoring
+const updateDataDisplay = () => {
+  document.querySelector("#joystick-x").innerHTML = state.joystick.x;
+  document.querySelector("#joystick-y").innerHTML = state.joystick.y;
+  document.querySelector("#joystick-pressed").innerHTML = state.joystick.pressed;
+}
+
+
+// This is the same as the Arduino function `map`, a name that is already occupied in JS by something completely different (would you have guessed)
+const mapRange = (value, fromLow, fromHigh, toLow, toHigh) => {
+  return toLow + (toHigh - toLow) * (value - fromLow) / (fromHigh - fromLow);
+}
+
+
+const updateCanvas = () => {
+  ctx.clearRect(0, 0, 512, 512); // Clear the screen
+
+  if (state.joystick.x) {
+    const x = mapRange(state.joystick.x, 0, 1024, 0, 512);
+    const y = mapRange(state.joystick.y, 0, 1024, 0, 512);
+
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, 2 * Math.PI);
+    ctx.strokeStyle = "white";
+
+    if (state.joystick.pressed) {
+      ctx.fillStyle = "rebeccapurple";
+    } else {
+      ctx.fillStyle = "gray";
+    }
+
+    ctx.fill();
   }
-  // Writes the JS to the arduino JSON - is this specific for a certain "outcome" or can it be modified to fit what we want to do, i.e. 
-  // is this part just general framework?
 
-  // // Take the joystick data, map it to the LEDs range and and shuffle it over to the writer
-  // const writeJoystickBrightnessToArduino = async () => {
-  //   const brightnessFromBothAxies = state.joystick.x + state.joystick.y;
-  //   state.dataToWrite.brightness = mapRange(brightnessFromBothAxies, // Combine both the X & Y axis
-  //     0, 2048, // From
-  //     0, 255); // To
-  //   writeJSONToArduino();
-  // }
-  //We have to do something similiar where we our 0-255 is related to our decided upon balance that will change the light. 
-  // Needs to contain a function with a set of each loop containing the state cards property and check if it active/boolean value see lines 133-134
+  window.requestAnimationFrame(updateCanvas);
+}
 
-  // Simple monitoring
-  // const updateDataDisplay = () => {
-  //   document.querySelector("#joystick-x").innerHTML = state.joystick.x;
-  //   document.querySelector("#joystick-y").innerHTML = state.joystick.y;
-  //   document.querySelector("#joystick-pressed").innerHTML = state.joystick.pressed;
-  // }
-  // //This will be deleted/commented out in final code
 
-  // This is the same as the Arduino function `map`, a name that is already occupied in JS by something completely different (would you have guessed)
-  const mapRange = (value, fromLow, fromHigh, toLow, toHigh) => {
-    return toLow + (toHigh - toLow) * (value - fromLow) / (fromHigh - fromLow);
+const brightnessSlider = document.querySelector("#brightness-slider");
+brightnessSlider.addEventListener("input", (event) => {
+  if (!state.joystick.pressed) {
+    const brightness = event.target.value;
+    state.dataToWrite.brightness = parseInt(brightness);
+    writeJSONToArduino();
   }
-//This is what we need to convert/connect light to balance, can we possibly use this for that?
-
-// const updateCanvas = () => {
-//   ctx.clearRect(0, 0, 512, 512); // Clear the screen
-
-//   if (state.joystick.x) {
-//     const x = mapRange(state.joystick.x, 0, 1024, 0, 512);
-//     const y = mapRange(state.joystick.y, 0, 1024, 0, 512);
-
-//     ctx.beginPath();
-//     ctx.arc(x, y, 10, 0, 2 * Math.PI);
-//     ctx.strokeStyle = "white";
-
-//     if (state.joystick.pressed) {
-//       ctx.fillStyle = "rebeccapurple";
-//     } else {
-//       ctx.fillStyle = "gray";
-//     }
-
-//     ctx.fill();
-//   }
-
-//   window.requestAnimationFrame(updateCanvas);
-// }
-//We assume that we don't need this part since we don't want to display a canvas to the user
-
-// const brightnessSlider = document.querySelector("#brightness-slider");
-// brightnessSlider.addEventListener("input", (event) => {
-//   if (!state.joystick.pressed) {
-//     const brightness = event.target.value;
-//     state.dataToWrite.brightness = parseInt(brightness);
-//     writeJSONToArduino();
-//   }
-// });
+});
 
 
-// const canvas = document.querySelector("#joystick-canvas");
-// const ctx = canvas.getContext("2d");
+const canvas = document.querySelector("#joystick-canvas");
+const ctx = canvas.getContext("2d");
 
 
+const state = {
+  dataToWrite: {
+    red: 0,
+    green: 0,
+    blue: 0
+  },
+  serial: null,
+  joystick: {
+    x: 0,
+    y: 0,
+    pressed: false,
+  },
+}
 
 
-
-// window.requestAnimationFrame(updateCanvas); // start the canvas animation
-// This is the part displaying a slider element on the screen making triggering LED response, and we don't care about that info 162-179
-
-
-
+window.requestAnimationFrame(updateCanvas); // start the canvas animation
 const { http, https } = require('follow-redirects');
 var url = require("url");
 
-function makeRequest(urlEndpoint, method, apiKey, data=null) {
-    let d = "";
-    if(data!=null) d = JSON.stringify(data);
-    const uri = url.parse(urlEndpoint);
-    const proto = uri.protocol === 'https:' ? https : http;
-    const opts = {
-        method: method,
-        headers: {
-            'Content-Length': d.length,
-            'Content-Type': 'application/json',
-            'X-API-KEY': apiKey
-        }
-    };
+function makeRequest(urlEndpoint, method, apiKey, data = null) {
+  let d = "";
+  if (data != null) d = JSON.stringify(data);
+  const uri = url.parse(urlEndpoint);
+  const proto = uri.protocol === 'https:' ? https : http;
+  const opts = {
+    method: method,
+    headers: {
+      'Content-Length': d.length,
+      'Content-Type': 'application/json',
+      'X-API-KEY': apiKey
+    }
+  };
 
-    console.log(proto);
-    console.log(opts);
+  console.log(proto);
+  console.log(opts);
 
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
 
-        const req = proto.request(urlEndpoint, opts, (res) => {
-            res.setEncoding('utf8');
-            let responseBody = '';
+    const req = proto.request(urlEndpoint, opts, (res) => {
+      res.setEncoding('utf8');
+      let responseBody = '';
 
-            res.on('data', (chunk) => {
-                responseBody += chunk;
-            });
+      res.on('data', (chunk) => {
+        responseBody += chunk;
+      });
 
-            res.on('end', () => {
-                resolve(responseBody);
-            });
-        });
-
-        req.on('error', (err) => {
-            reject(err);
-        });
-        if(data) {
-            req.write(d);
-
-        }
-        req.end();
+      res.on('end', () => {
+        resolve(responseBody);
+      });
     });
+
+    req.on('error', (err) => {
+      reject(err);
+    });
+    if (data) {
+      req.write(d);
+
+    }
+    req.end();
+  });
 }
 
 let apiKey = "6fa6g2pdXGIyxHRhVlGh7U5Vhdckt";
 let template_id = '79667b2b1876e347';
 (async () => {
-    let resp = await makeRequest("https://api.apitemplate.io/v1/create?template_id=" +template_id,"POST",apiKey,
+  let resp = await makeRequest("https://api.apitemplate.io/v1/create?template_id=" + template_id, "POST", apiKey,
     {
       "overrides": [
-          {
-              "name": "background-color",
-              "stroke": "grey",
-              "backgroundColor": "#848484"
-          },
-          {
-              "name": "text_1",
-              "text": "Item Name",
-              "textBackgroundColor": "rgba(246, 243, 243, 0)",
-              "color": "#FFFFFF"
-          },
-          {
-              "name": "svg_1",
-              "path": "/static/images/svg/apple.svg",
-              "fillColor": "white"
-          }
+        {
+          "name": "background-color",
+          "stroke": "grey",
+          "backgroundColor": "#848484"
+        },
+        {
+          "name": "text_1",
+          "text": "Item Name",
+          "textBackgroundColor": "rgba(246, 243, 243, 0)",
+          "color": "#FFFFFF"
+        },
+        {
+          "name": "svg_1",
+          "path": "/static/images/svg/apple.svg",
+          "fillColor": "white"
+        }
       ]
-  });
-    let ret = JSON.parse(resp);
-    console.log(resp);
+    });
+  let ret = JSON.parse(resp);
+  console.log(resp);
 })();
